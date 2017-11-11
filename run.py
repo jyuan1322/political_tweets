ad2dd530332cc7c55ae2138c83c7172a7101c1f0
Jie Yuan <jyuan@cs.columbia.edu>
2017-11-11 22:10:00 +0000

    Automatic refresh using Ajax for word clouds. To do: only update if new tweets are detected

--- run.py
+++ run.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 import json
-from flask import Flask,render_template,request
+from flask import Flask,render_template,request,jsonify
 from sqlalchemy import *
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 from pprint import pprint
 
 app = Flask(__name__)
@@ -46,30 +47,55 @@ def network():
 @app.route('/twitter_word_cloud', methods=['GET','POST'])
 def get_word_cloud():
     if request.method == 'POST':
-        sql = """
-            SELECT A.handle,B.word,COUNT(B.word)
-            FROM
-                    (SELECT TU.handle, WW.political_party
-                    FROM twitter_user TU, wikipedia_page WW
-                    WHERE TU.handle = WW.handle) A
-            JOIN
-                    (SELECT DISTINCT T.handle,T.tweet_id,TW.word
-                    FROM tweet T, twitter_word TW
-                    WHERE T.tweet_id = TW.tweet_id) B
-            ON A.handle = B.handle
-            WHERE A.handle = '%s'
-            GROUP BY A.handle,B.word;
-        """ % (request.form["twitter_handle"])
-        res = engine.execute(sql)
-        res = [dict(x) for x in res]
-        words = []
+        res,words = word_cloud_sql(request.form["twitter_handle"])
+
+        res_str = []
         for r in res:
-            words.append({'text':str(r['word']),'size':int(r['count'])})
+            for i in range(int(r['count'])):
+                res_str.append(r['word'])
+        res_str = " ".join(res_str)
+        analyzer = SentimentIntensityAnalyzer()
+        sentiment = analyzer.polarity_scores(res_str)["compound"]
+
+        print "selected:", request.form["twitter_handle"]
+
         return render_template('user_word_cloud.html',
                                 twitter_user=request.form["twitter_handle"],
+                                sentiment=sentiment,
                                 words=words)
     else:
         return render_template('user_word_cloud.html',twitter_user='',words=None)
+
+def word_cloud_sql(twitter_handle):
+    sql = """
+        SELECT A.handle,B.word,COUNT(B.word)
+        FROM
+                (SELECT TU.handle, WW.political_party
+                FROM twitter_user TU, wikipedia_page WW
+                WHERE TU.handle = WW.handle) A
+        JOIN
+                (SELECT DISTINCT T.handle,T.tweet_id,TW.word
+                FROM tweet T, twitter_word TW
+                WHERE T.tweet_id = TW.tweet_id) B
+        ON A.handle = B.handle
+        WHERE A.handle = '%s'
+        GROUP BY A.handle,B.word;
+    """ % (twitter_handle)
+    res = engine.execute(sql)
+    res = [dict(x) for x in res]
+    words = []
+    for r in res:
+        words.append({'text':str(r['word']),'size':int(r['count'])})
+    return res,words
+
+import datetime
+@app.route('/get_values', methods=['GET','POST'])
+def get_values():
+    twitter_handle = request.get_data()
+    print "twitter_handle:", twitter_handle
+    result = "this is a test" + datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    res, new_wc_words = word_cloud_sql(twitter_handle)
+    return jsonify(result=result,new_wc_words=new_wc_words)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0',port=5000)
